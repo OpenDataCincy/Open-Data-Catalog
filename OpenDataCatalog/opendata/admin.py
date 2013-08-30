c903f1d3a34f0b2409bc4148a9bf2fbe9bd0c051
John Back <johndavidback@gmail.com>
2013-08-30 13:43:13 -0400

    PEP8

--- OpenDataCatalog/opendata/admin.py
+++ OpenDataCatalog/opendata/admin.py
@@ -5,19 +5,23 @@ from OpenDataCatalog.suggestions.models import *
 from OpenDataCatalog.contest.models import *
 from django.contrib import admin
 
+
 class UrlImageInline(admin.TabularInline):
     model = UrlImage
     extra = 1
-    
+
+
 class UrlInline(admin.TabularInline):
     model = Url
     extra = 1
     verbose_name = 'Resource Url'
     verbose_name_plural = 'Resource Urls'
 
+
 class IdeaImageInline(admin.TabularInline):
     model = IdeaImage
     extra = 1
+
 
 class ResourceAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -48,21 +52,26 @@ class ResourceAdmin(admin.ModelAdmin):
         obj.last_updated_by = request.user
         obj.save()
 
+
 class UrlImageAdmin(admin.ModelAdmin):
     list_display = ('title', 'image')
     search_fields = ['image', 'title', 'description']
-    
+
+
 class UrlAdmin(admin.ModelAdmin):
     list_display = ('url_label', 'url_type', 'url')
     inlines = [UrlImageInline,]
     list_filter = ['url_type',]
-    
+
+
 class CoordSystemAdmin(admin.ModelAdmin):
     list_display = ('EPSG_code', 'name')
     search_fields = ['name', 'EPSG_code', 'description']
 
     verbose_name = 'Resource Url'
     verbose_name_plural = 'Resource Urls'
+
+
 class IdeaAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields':[('title', 'author'),  'description', ('created_by', 'created_by_date'), 
@@ -82,9 +91,11 @@ class IdeaAdmin(admin.ModelAdmin):
         obj.updated_by = request.user
         obj.save()
 
+
 class SuggestionAdmin(admin.ModelAdmin):
     list_display = ['text', 'suggested_by', 'completed']
     search_fields = ['text', 'suggested_by']
+
 
 class SubmissionAdmin(admin.ModelAdmin):   
     verbose_name = 'Resource Url'
@@ -99,31 +110,36 @@ class SubmissionAdmin(admin.ModelAdmin):
         
         obj.save()
 
+
 class ODPUserProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'can_notify',]
     fieldsets = [(None, {'fields':['user', 'organization', 'can_notify']}),]
     readonly_fields = ['user',]
     list_filter = ['can_notify',]
-    
+
+
 class EntryAdmin(admin.ModelAdmin):
     list_display = ['title', 'nominator', 'contest']
     search_fields = ['title', 'nominator', 'description']
     list_filter = ['contest__title', ]
+
 
 class EntryInline(admin.StackedInline):
     model = Entry
     extra = 1
     verbose_name_plural = 'Entries'
 
+
 class ContestAdmin(admin.ModelAdmin):
     list_display = ['title', 'start_date', 'end_date']
     search_fields = ['title', 'rules']
     inlines = [EntryInline, ]
 
+
 class VoteAdmin(admin.ModelAdmin):
     list_display= ['entry', 'user', 'timestamp']
     search_fields = ['entry']
-    list_filter = ['entry',]
+    list_filter = ['entry', ]
 
 admin.site.register(Submission, SubmissionAdmin)
 admin.site.register(ODPUserProfile, ODPUserProfileAdmin)
