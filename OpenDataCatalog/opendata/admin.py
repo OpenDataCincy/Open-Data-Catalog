f4b15f4040cc4058c8828e18e8b7ff6f0a1dbf32
John Back <johndavidback@gmail.com>
2013-09-03 21:00:19 -0400

    Unused import removed

--- OpenDataCatalog/opendata/admin.py
+++ OpenDataCatalog/opendata/admin.py
@@ -8,7 +8,6 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import PasswordResetForm
 from django.contrib.auth.tokens import default_token_generator
-from django.conf import settings
 
 
 class UserAdmin(UserAdmin):
