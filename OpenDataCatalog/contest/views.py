1584d744c30aeaab8e4504a08c3602000c722c10
John Back <johndavidback@gmail.com>
2013-09-13 18:01:14 -0400

    I meant to remove that before, but now removing the old add entry method

--- OpenDataCatalog/contest/views.py
+++ OpenDataCatalog/contest/views.py
@@ -103,44 +103,6 @@ def get_entry(request, entry_id):
     return render_to_response('contest/entry.html', {'contest': entry.contest, 'entry': entry}, context_instance=RequestContext(request))
 
 
-#@login_required
-def add_entry(request, contest_id=1):
-    contest = Contest.objects.get(pk=contest_id)
-    if request.method == 'POST':
-        form = EntryForm(request.POST)
-        form.contest = contest_id
-
-        if form.is_valid():
-
-            data = {
-                #"submitter": request.user.username,
-                "submit_date": datetime.now(),
-                "org_name": request.POST.get("org_name"),
-                "org_url": request.POST.get("org_url"),
-                "contact_person": request.POST.get("contact_person"),
-                "contact_phone": request.POST.get("contact_phone"),
-                "contact_email": request.POST.get("contact_email"),
-                "data_set": request.POST.get("data_set"),
-                "data_use": request.POST.get("data_use"),
-                "data_mission": request.POST.get("data_mission")
-            }
-
-            subject = 'OpenDataCincy - Contest Submission'
-            user_email = request.POST.get("contact_email")
-            text_content = render_to_string('contest/submit_email.txt', data)
-            text_content_copy = render_to_string('contest/submit_email_copy.txt', data)
-            mail_managers(subject, text_content)
-
-            msg = EmailMessage(subject, text_content_copy, to=[user_email])
-            msg.send()
-
-            return render_to_response('contest/thanks.html', {'contest': contest}, context_instance=RequestContext(request))
-
-    else: 
-        form = EntryForm()
-
-    return render_to_response('contest/submit_entry.html', {'contest': contest, 'form': form}, context_instance=RequestContext(request))
-
 @login_required
 def add_vote(request, entry_id):
     entry = Entry.objects.get(pk=entry_id)
