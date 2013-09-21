43069dbf011047259c88147ce7f7403d06d09876
John Back <johndavidback@gmail.com>
2013-09-21 11:27:25 -0400

    Adding in the config so that we don't intercept redirects

--- OpenDataCatalog/settings.py
+++ OpenDataCatalog/settings.py
@@ -139,7 +139,11 @@ MIDDLEWARE_CLASSES = (
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
+# Debug toolbar settings.
 INTERNAL_IPS = ('127.0.0.1',)
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+}
 
 ROOT_URLCONF = 'OpenDataCatalog.urls'
 
