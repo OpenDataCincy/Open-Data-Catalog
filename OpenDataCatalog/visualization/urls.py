a355ebbb2d45e2d05ce2ead0e56fef015fd904b8
John Back <johndavidback@gmail.com>
2013-11-07 14:10:55 -0500

    Now we have a nice base for the google maps stuff

--- OpenDataCatalog/visualization/urls.py
+++ OpenDataCatalog/visualization/urls.py
@@ -1,1 +1,6 @@
-__author__ = 'johnback'
+from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
+
+urlpatterns = patterns('',
+    url(r'^map/$', TemplateView.as_view(template_name='visualization/map.html'), name='visualization-map'),
+)
