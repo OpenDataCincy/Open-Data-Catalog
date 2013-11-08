f455e78132701347cb40c1d8c4732d87ea89e249
John Back <johndavidback@gmail.com>
2013-11-08 13:43:37 -0500

    Now we can plot thigns right on that crazy old map

--- OpenDataCatalog/opendata/management/commands/311.py
+++ OpenDataCatalog/opendata/management/commands/311.py
@@ -2,6 +2,8 @@ from django.core.management.base import BaseCommand, CommandError
 
 import xlrd
 
+from geopy import geocoders
+
 
 class Command(BaseCommand):
     help = "Supply an xls or xlsx file to be parsed for Crime Data"
@@ -19,7 +21,12 @@ class Command(BaseCommand):
         # We just want the first sheet.
         sheet = workbook.sheet_by_index(0)
 
+        # The geocoder
+        google = geocoders.GoogleV3()
+
         # # Go through each row and handle.
+        count = 0
+        addresses = {}
         for i in range(sheet.nrows):
             row = sheet.row_values(i)
 
@@ -30,4 +37,29 @@ class Command(BaseCommand):
             # row[4] is the date MM/DD/YYYY HH:MM:SS AM
 
             if 'trash' in row[2]:
-                print row[0]
+                count += 1
+
+                if row[7]:
+                    address = u'%s Cincinnati, OH' % row[7]
+
+                    if not address in addresses:
+                        place, (lat, lon) = google.geocode(address)
+                        addresses[address] = {
+                            'lat': lat,
+                            'lon': lon,
+                        }
+                    else:
+                        lat = addresses[address].get('lat')
+                        lon = addresses[address].get('lon')
+
+                    js = 'var myLatLng = new google.maps.LatLng(%s, %s); ' % (lat, lon)
+                    js += 'var marker = new google.maps.Marker({ position: myLatLng, title: "%s" }); ' % row[2]
+                    js += 'marker.setMap(map);'
+
+                    self.stdout.write(js)
+                    self.stdout.write("\n")
+
+                if count == 100:
+                    break
+
+        print count
