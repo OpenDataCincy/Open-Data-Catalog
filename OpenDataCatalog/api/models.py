b47235d2e071734c11841c0f778edfc8bc042f50
John Back <johndavidback@gmail.com>
2013-11-13 17:36:06 -0500

    This stuff is now owrking great

--- OpenDataCatalog/api/models.py
+++ OpenDataCatalog/api/models.py
@@ -13,7 +13,7 @@ class ThreeOneOne(models.Model):
     community = models.CharField(max_length=100, blank=True)
     census_tract = models.FloatField(null=True, blank=True)
     priority = models.CharField(max_length=20, blank=True)
-    method = models.CharField(max_length=20, blank=True)
+    method = models.CharField(max_length=50, blank=True)
     parcel_number = models.CharField(max_length=30, blank=True, help_text=u'Land parcel number')
     date_answered = models.DateField()
     user_id = models.CharField(max_length=20)
@@ -33,14 +33,14 @@ class ThreeOneOne(models.Model):
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
 
-        if self.latitude == 0 and self.longitude == 0:
-
-            address = '%s Cincinnati, OH' % self.street_address
-
-            google = geocoders.GoogleV3()
-            place, (lat, lon) = google.geocode(address, exactly_one=False)[0]
-
-            self.latitude = lat
-            self.longitude = lon
+        # if self.latitude == 0 and self.longitude == 0:
+        #
+        #     address = '%s Cincinnati, OH' % self.street_address
+        #
+        #     google = geocoders.GoogleV3()
+        #     place, (lat, lon) = google.geocode(address, exactly_one=False)[0]
+        #
+        #     self.latitude = lat
+        #     self.longitude = lon
 
         return super(ThreeOneOne, self).save(force_insert, force_update, using, update_fields)
