24333443fb67ad847ad4f37249117d06f0f2dfe1
John Back <johndavidback@gmail.com>
2013-11-15 11:13:38 -0500

    Now we can dynamically place the points on the map

--- OpenDataCatalog/api/serializers.py
+++ OpenDataCatalog/api/serializers.py
@@ -6,4 +6,4 @@ from rest_framework import serializers
 class ThreeOneOneSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ThreeOneOne
-        fields = ('csr', 'status', 'date_received', )
+        fields = ('csr', 'status', 'date_received', 'latitude', 'longitude')
