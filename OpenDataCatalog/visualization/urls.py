a1eecf2e1f57d0eee720f242b537ed80037f90e0
John Back <johndavidback@gmail.com>
2013-12-19 11:36:31 -0500

    Starting visualization for CPD

--- OpenDataCatalog/visualization/urls.py
+++ OpenDataCatalog/visualization/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
+from .views import CPDBarChartView
+
 urlpatterns = patterns('',
     url(r'^map/$', TemplateView.as_view(template_name='visualization/map.html'), name='visualization-map'),
+    url(r'^cpd/$', CPDBarChartView.as_view(), name='visualization-cpd-bar'),
 )
