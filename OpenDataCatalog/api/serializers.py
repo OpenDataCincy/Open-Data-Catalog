ff15339586e34042956d3ff386c30ec8f7763000
John Back <johndavidback@gmail.com>
2014-01-09 14:28:09 -0500

    Now it's a speck smarter, adding badge number

--- OpenDataCatalog/api/serializers.py
+++ OpenDataCatalog/api/serializers.py
@@ -25,4 +25,5 @@ class CincinnatiPoliceSerializer(serializers.HyperlinkedModelSerializer):
 class ArrestSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Arrest
-        fields = ['arrest_type', 'event_date', 'event_time', 'dob_year', 'charge_code', 'anon_arrest_address', ]
+        fields = ['arrest_type', 'event_date', 'event_time', 'dob_year', 'charge_code', 'anon_arrest_address',
+                  'badge_number']
