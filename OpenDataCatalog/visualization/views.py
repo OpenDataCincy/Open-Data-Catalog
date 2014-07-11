40d4a5e2139a857f767f9ff4445daaa6dcabe5a9
John Back <johndavidback@gmail.com>
2014-07-11 12:48:35 -0400

    Holy hell, well, now we have this crazy listing of transit data

--- OpenDataCatalog/visualization/views.py
+++ OpenDataCatalog/visualization/views.py
@@ -1,6 +1,10 @@
 # Create your views here.
 from django.views.generic import TemplateView
 
+from boto.s3.connection import S3Connection
+
+from django.conf import settings
+
 
 class CPDBarChartView(TemplateView):
     template_name = 'visualization/cpd-bar-chart.html'
@@ -11,4 +15,19 @@ class CPDBarChartView(TemplateView):
 
         return {
 
+        }
+
+
+class TransitDataView(TemplateView):
+    template_name = 'visualization/transit.html'
+
+    def get_context_data(self, **kwargs):
+
+        # These are stored in local_settings.py, but should be set as environment variables
+        conn = S3Connection(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
+
+        bucket = conn.get_bucket('opendatacincy')
+
+        return {
+            'list': bucket.list(),
         }
